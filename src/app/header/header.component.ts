1f0b990c8d5ae56adcb0e04ae6cb392767960464
Ngoc Quach <nquach@mii.com>
2022-03-11 17:00:47 +0700

    update1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public name = 'abc';
+  public age = 17;
+  public vehicles = ['toyota', 'honda'];
+
   constructor() { }
+
+  public tangTuoi(){
+    this.age++;
+    //this.vehicles.push(this.name + ': ' + this.age);
+  }
+
+  public giamTuoi(){
+    this.age--;
+  }
 
   ngOnInit(): void {
   }
