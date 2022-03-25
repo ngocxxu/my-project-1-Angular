12ea6c9bcd11b0212d754880f74983e8a807407d
Ngoc Quach <nquach@mii.com>
2022-03-25 15:11:17 +0700

    backup material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { CommonService } from './service/common.service';
+import { ServerHTTPService } from './service/server-http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  title = 'HelloWorld';
+  @ViewChild('sidenav') sidenav?: MatSidenav;
+  public isOpened = false;
+  public totalStudents = 0;
+
+  constructor(
+    private common: CommonService,
+    private serverHttp: ServerHTTPService
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+  public openLeftSide() {
+    this.isOpened = !this.isOpened;
+    this.sidenav?.toggle();
+  }
+
+  public closeLeftSide() {
+    this.isOpened = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -20,13 +29,24 @@ import { ReactiveComponent } from './reactive/reactive.component';
     PageNotFoundComponent,
     BoldDirective,
     FormComponent,
-    ReactiveComponent
+    ReactiveComponent,
   ],
-  imports: [HttpClientModule,
+  imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule, FormsModule, ReactiveFormsModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
